cf2905548133a86d7efbcb8a1fb2d810da2af478
Demi Nguyen <deminguy@usc.edu>
2013-10-29 15:13:17 -0700

    print cat for numbers divisible by 10

--- FizzBuzz.java
+++ FizzBuzz.java
@@ -1,7 +1,10 @@
 public class FizzBuzz{
 	public static void main(String[]args){
 		for (int i = 0; i <100; i++){
-			System.out.println(i);
+			if(i%10 == 0)
+				System.out.println("Cat");
+			else
+				System.out.println(i);
 		}
 	}
 }
